e76029322ac0b8f10c42ee67c0220122680ec6b6
GiorgioBolzoni <gbolzoni96@gmail.com>
2023-11-07 17:22:23 +0100

    generate 10 mail e stampate in pagina - esercizio base completato

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
-
-
 const { createApp } = Vue;
 
 createApp({
     data(){
         return {
-
+            mails: []
         }
     },
     methods: {
         
     },
     mounted() {
-        
+        for(let i = 0; i < 10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp)=> {
+                 console.log(resp.data);
+                 this.mails.push(resp.data.response);
+               })
+        }
     },
 }).mount('#app')
